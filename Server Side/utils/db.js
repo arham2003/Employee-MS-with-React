5fee0c310ba98fb8cc05fcb7ef2abdd7e39c9fb4
Partham Kumar <parthamchawla@gmail.com>
2024-11-15 12:25:13 +0500

    InitialSetup

---
+++ Server Side/utils/db.js
@@ -0,0 +1,19 @@
+import mysql from 'mysql';
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "employeems",
+    port: 5713  // Specify the correct port
+});
+
+con.connect(function(err) {
+    if (err) {
+        console.error("Connection error: ", err);
+    } else {
+        console.log("Connected");
+    }
+});
+
+export default con;
