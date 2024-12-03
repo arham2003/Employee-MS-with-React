5b18c8757bb72b7031b616bac4bddaec10ded75d
arham2003 <arhamkx@gmail.com>
2024-12-04 02:27:23 +0500

    dotenv package issue

--- Front-End/src/Components/Start.jsx
+++ Front-End/src/Components/Start.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-useEffect
 
 const Start = () => {
     const navigate = useNavigate()
