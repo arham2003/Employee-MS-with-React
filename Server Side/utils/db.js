ba3361909700b59831ea84db60a0e498d85f4847
arham2003 <arhamkx@gmail.com>
2024-12-03 23:28:04 +0500

    Changed db conn to mysql2

--- Server Side/utils/db.js
+++ Server Side/utils/db.js
@@ -1,4 +1,4 @@
-import mysql from 'mysql';
+import mysql from 'mysql2';
 import 'dotenv/config';
 
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE} `
