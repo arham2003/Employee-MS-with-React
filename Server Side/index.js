e2aafd9255acd0332e95b37f03beb197530019ec
arham2003 <arhamkx@gmail.com>
2024-12-04 18:40:18 +0500

    reversed all origins in Cors

--- Server Side/index.js
+++ Server Side/index.js
@@ -23,11 +23,17 @@ const allowedOrigins = [
   ];
   
   app.use(cors({
-    origin: '*',
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }));
-
+  
 app.use(express.json())
 // app.use(bodyParser.json());
 app.use(cookieParser())
