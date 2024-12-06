01720307d31a8e21969140ba36b2e7d1ee734b8b
arham2003 <arhamkx@gmail.com>
2024-12-06 20:09:39 +0500

    added more link to cors 2

--- Server Side/index.js
+++ Server Side/index.js
@@ -20,7 +20,9 @@ const app = express()
 const allowedOrigins = [
     "http://localhost:3000", // Local development
     "https://employee-ms-with-react.vercel.app", // Deployed frontend
-    "https://employee-ms-with-react.vercel.app/dashboard", 
+    "https://employee-ms-with-react.vercel.app/dashboard",
+    "https://employee-ms-with-react-production.up.railway.app",
+    "https://employee-ms-with-react.vercel.app/dashboard/add_employee",
   ];
   
   app.use(cors({
