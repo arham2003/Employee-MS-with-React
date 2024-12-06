f9627e93f62853d03f71664e9598926577196972
arham2003 <arhamkx@gmail.com>
2024-12-06 20:04:05 +0500

    added more link to cors

--- Server Side/index.js
+++ Server Side/index.js
@@ -19,7 +19,8 @@ const app = express()
 
 const allowedOrigins = [
     "http://localhost:3000", // Local development
-    "https://employee-ms-with-react.vercel.app" // Deployed frontend
+    "https://employee-ms-with-react.vercel.app", // Deployed frontend
+    "https://employee-ms-with-react.vercel.app/dashboard", 
   ];
   
   app.use(cors({
